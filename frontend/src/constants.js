2f48eace3eeb8599fc50a1aadf18ff5b8cc5cd8a
IhsanuRahman <=ihsanurahman8055@gmail.com>
2024-07-04 17:00:56 +0530

    1.0

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,2 +1,2 @@
-export const baseUrl='http://3.110.55.148'
-export const WSBaseUrl='ws://3.110.55.148'
+export const baseUrl='https://mediacapital-server.space'
+export const WSBaseUrl='ws://mediacapital-server.space'
